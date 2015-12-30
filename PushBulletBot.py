0ad9b619f25f4212c087396ba2ad183ed5d9a015
Riley Durant <rdrnt@Rileys-iMac.local>
2015-12-29 22:18:43 -0500

    Fixed code

--- PushBulletBot.py
+++ PushBulletBot.py
@@ -6,58 +6,46 @@ from datetime import datetime
 import praw
 
 #Fill out this info
-weatherCode = 'InsertWeatherCode'
-name = 'PutYourNameHere'
+weatherCode = 'YourWeatherCode'
+name = 'YourName'
 
 #Pushbullet API Key
-api_key = 'PutYourAPIKeyHere'
+api_key = 'yourApiKeyHere'
 
 def pushGoodMorning():
-
-	#time info
-	now = datetime.now().time()
-	print ('The hour is ', now.hour)
-	print ('The minute rn is ', now.minute)
-	#This line is used for determining AM or PM
-	mornAfternoon = datetime.now().strftime('%p')
-
-
-	#Connected Devices
-	#print ("The current devices are", pb.devices)
-
-	#getting reddit info
-	user_agent = "Comment Reader 1.0 by /u/x9a"
-	r = praw.Reddit(user_agent=user_agent)
-	submissions = r.get_subreddit('news').get_hot(limit=1)
-	for x in submissions:
-		print str(x.title)
-		topTitle = str(x.title)
-
-	#weather info
-	weather_results = pywapi.get_weather_from_weather_com(weatherCode)
-	print ("\n The weather outside is: " + weather_results['current_conditions']['text'] + "& The temp is " + weather_results['current_conditions']['temperature'] + "C" + "\n")
-	weather_saying = ("The weather is " + weather_results['current_conditions']['text'])
-	#weather_saying = ("The weather is " + weather_results['current_conditions']['text'] + "  & The temperature outside is " + weather_results['current_conditions']['temperature'] + "C.")
-
-	#pushbullet info
+	#PushBullet API
 	pb = Pushbullet(api_key)
 
-	#If you dont know 24 hour clocks, refer to this = http://www.officeclocks.com/home/Html/Images/st1143.jpg
-	#Here we push the stuff at 7:30 AM.
-	if now.hour == 19 and now.minute == 30 and mornAfternoon == 'AM':
-		push = pb.push_note(name + ", it's currently " + str(now.hour-12) + ":" + str(now.minute) + mornAfternoon + ".",  weather_saying)
-		print ("Pushed the notification, have a great day!")
-	else:
-		print ("The time doesnt match up, so we're gonna try again soon.")
+	#getting time
+	now = datetime.now().time()
+	#AM or PM
+	mornAfternoon = datetime.now().strftime('%p')
 
-	if now.hour == 22 and now.minute == 24 and mornAfternoon == 'PM':
-		push = pb.push_note("Riley, the latest news is:",  topTitle)
-		print ("Sent the top title from today, enjoy it!")
+	#Here we push the notifcation(s)
+	if now.hour == 6 and now.minute == 30:
+		#Getting the weather
+		weather_results = pywapi.get_weather_from_weather_com(weatherCode)
+		weather_saying = ("The weather is " + weather_results['current_conditions']['text'])
+		#pushing
+		push = pb.push_note(name + ", it's currently " + str(now.hour-12) + ":" + str(now.minute) + mornAfternoon + ".",  weather_saying)
 	else:
-		print ("Reddit: The time doesnt match up, so we're gonna try again.")
+		#Put something else here
+		print "Oh darn, the time is off for the weather, we'll try again soon"
+
+	
+	if now.hour == 22 and now.minute == 17:
+		#getting reddit post title
+		user_agent = "Bot that grabs top title in /r/ news"
+		r = praw.Reddit(user_agent=user_agent)
+		submission = r.get_subreddit('news').get_hot(limit=1)
+		for x in submission:
+			topTitle = str(x.title)
+		#Pushing
+		push = pb.push_note(name+  ", the latest news is:",  topTitle)
+	else:
+		print "Uh oh! The time didnt match up for your reddit post, we'll try again later"
 
 while True:
-	print("Sleeping...")
 	sleep(45)
 	pushGoodMorning()
 
